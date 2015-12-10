34d9176d643cf0d3239cadc596f7e6e4b6135772
18jelias <jacquie19_e@yahoo.com>
2015-12-10 02:37:05 -0600

    is file valid

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,7 +1,7 @@
 #! /usr/bin/env ruby
 
 
-if (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1)
+if (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1 && File.readable?(ARGV[0]) && File.exist?(ARGV[0]))
   asm_filename=ARGV[0]
   puts asm_filename
 else
