0c3933757b83847291973e4ea0a7f34361cedcfe
18jelias <jacquie19_e@yahoo.com>
2015-12-10 02:39:28 -0600

    not readable error

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,7 +1,7 @@
 #! /usr/bin/env ruby
 
 
-if (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1 && File.readable?(ARGV[0]) && File.exist?(ARGV[0]))
+if (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1)
   asm_filename=ARGV[0]
   puts asm_filename
 else
@@ -9,3 +9,7 @@ else
 end
 
 
+if(File.readable?(asm_filename) && File.exist?(asm_filename)
+	puts "The contents of #{asm_filename}"
+else
+	abort("#{asm_filename} is not readable.")
