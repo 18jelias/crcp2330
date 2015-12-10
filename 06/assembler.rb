6a16aba095272b3b41727faf92a1f2a6e08cb384
18jelias <jacquie19_e@yahoo.com>
2015-12-10 10:15:05 -0600

    Saves if valid and readable and arg valid

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,15 +1,22 @@
 #! /usr/bin/env ruby
 
 
-if (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1)
-  asm_filename=ARGV[0]
-  puts asm_filename
-else
-	abort("Usage: ./assembler.rb Prog.asm")
+def args_valid?
+	return (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1)
+end 
+
+def is_readable?(path)
+	File.readable?(path)
 end
 
+unless args_valid?
+	abort("Usage: ./assembler.rb Prog.asm")
+end 
 
-if(File.readable?(asm_filename) && File.exist?(asm_filename)
+asm_filename = ARGV[0]
+
+unless(is_readable?(asm_filename))
+	abort("#{asm_filename} is not found or is unreadable readable.")
+end
 	puts "The contents of #{asm_filename}"
-else
-	abort("#{asm_filename} is not readable.")
+
