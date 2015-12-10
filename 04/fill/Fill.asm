936bbce46c7c490ebaf53c0e5dd5adbffe2382de
18jelias <jacquie19_e@yahoo.com>
2015-12-09 22:43:38 -0600

    New programs

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -8,4 +8,76 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+(Loop)
+	//@8192
+	//D=A
+	//@pixelCount //Number of pixels on screen
+	//M=D
+
+	@nextPixel //Counts up
+	M=0
+
+	@KBD //Checks what keyboard input is 
+	D=M
+
+	@Clear //Jumps to clear if 0
+	D;JEQ
+
+	@Dark //Jumps to dark if not 0
+	D;JNE
+
+(Dark)
+	@nextPixel
+	D=M
+	@KBD //Compares address of KBD to address of current counter
+	D=A-D
+	@Loop
+	D;JEQ
+
+	@nextPixel
+	D=M
+	@SCREEN
+	A=A+D
+	M=-1 //Fills dark 
+
+	@nextPixel
+	M=M+1
+	D=M
+	@SCREEN
+	D=A+D
+
+	@KBD //Compares address of KBD to address of current counter
+	D=A-D
+	@Dark
+	D;JNE
+
+	@Loop
+	D;JEQ
+
+(Clear)
+	@nextPixel
+	D=M
+	@KBD //Compares address of KBD to address of current counter
+	D=A-D
+	@Loop  
+	D;JEQ
+
+	@nextPixel
+	D=M
+	@SCREEN
+	A=A+D
+	M=0 //Fills dark 
+
+	@nextPixel
+	M=M+1
+	D=M
+	@SCREEN
+	D=A+D
+
+	@KBD //Compares address of KBD to address of current counter
+	D=A-D
+	@Clear
+	D;JNE
+
+	@Loop
+	D;JEQ
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,43 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+	@R1 //Load multiplier  into A
+	D=M //Move multiplier into D
+	@count //Allocate memory for counter
+	M=D //Load multiplier into counter
+
+
+	@R0 //Load multicand into A
+	D=M //Move multicand into D
+	@multicand //Allocate memory for multicand 
+	M=D //Load multicand into multicand 
+
+	@R2
+	M=0 //Initialize product
+
+(Loop)
+	@count //Load counter into A
+	D=M //Move counter into D
+
+	@END
+	D;JEQ //Tests if counter = 0
+
+	@R2
+	D=M //Save answer into D
+	@multicand //Load what you are adding
+	D=D+M //Increment
+
+	@R2 //Store added value 
+	M=D //Loads value into answer
+
+	@count //Load counter
+	D=M //Decrement counter
+	D=D-1
+	M=D //Move counter to D
+
+	@Loop
+	0;JMP //Loops to top of loop
+
+(END)
+	@END
+	0;JMP
